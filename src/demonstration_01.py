92747dbe60075a154e3c472f4b46b8719bcb61b5
Anna Brander <66019108+aelise17264@users.noreply.github.com>
2020-10-27 11:58:54 -0600

    Update demonstration_01.py

--- src/demonstration_01.py
+++ src/demonstration_01.py
@@ -2,7 +2,7 @@
 Challenge #1:
 
 Write a function that retrieves the last n elements from a list.
-
+we want the last x amount of digits
 Examples:
 - last([1, 2, 3, 4, 5], 1) ➞ [5]
 - last([4, 3, 9, 9, 7, 6], 3) ➞ [9, 7, 6]
@@ -17,4 +17,17 @@ Notes:
 
 def last(a, n):
     # Your code here
-
+    # if n is longer than the length of the list it should return invalid
+    # we need to return the last n elements as a list of ints
+    #how would we do this is we just needed to return the first n elements?
+    #we'd want to grab everything starting from index 0 up to n
+    # we'd want to grab a subslice where that subslice starts at
+    # the beginning of our input list has length n
+    if n > len(a):
+        return "invalid"
+    last_n = a[len(a) -n:]
+    return n
+print (last([1, 2, 3, 4, 5], 1))
+print ( last([4, 3, 9, 9, 7, 6], 3))
+print (last([1, 2, 3, 4, 5], 7))
+print (last([1, 2, 3, 4, 5], 0))
