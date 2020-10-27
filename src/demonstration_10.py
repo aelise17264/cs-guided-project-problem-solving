4f12668f37056a7f0e48a60a677d67446b2b7265
Anna Brander <66019108+aelise17264@users.noreply.github.com>
2020-10-27 12:01:24 -0600

    Update demonstration_10.py

--- src/demonstration_10.py
+++ src/demonstration_10.py
@@ -17,5 +17,22 @@ the maximum number is first.
 """
 def max_and_min(input_str):
     # Your code here
+    #given a string of numbers figure out the min & max
+    # we have functions max & min built in
+    # but these only work for numbers not strings
+    # pass works on non negative numeric strings
+    # b/c of negative numbers we can't get away w/ not turning the string into numbers
+    # we need to transform it into a list of numbers, then we can use max & min
+    str_digits = input_str.split(" ")
+    int_digits = []
 
+    for str_digit in str_digits:
+        int_digit = int(str_digit)
+        int_digits.append(int_digit)
+    mx = max(int_digits)
+    mn = min(int_digits)
 
+    return f"{mx} {mn}"
+
+print (max_and_min("1 2 3 4 5"))
+print(max_and_min("1 9 3 4 -5"))
